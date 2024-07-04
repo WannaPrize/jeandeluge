7ca9856a131e8c9332186b25758b77f82aa5b1da
jeandelgue <jeandelugedev@gmail.com>
2024-07-04 14:52:39 +0900

    feat: pytorch-template에서 NewProject 생성

---
+++ NewProject/base/__init__.py
@@ -0,0 +1,7 @@
+from .base_data_loader import *
+from .base_model import *
+from .base_trainer import *
+###
+#base_data_loader.py, base_model.py, base_trainer를 각각 임포트할 필요업이
+# base 에서 한꺼번에 불러 올 수 있도록 하기 위함
+### 
